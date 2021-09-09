53929e0b7e1afbcd911761f6ce5c0aa1c44b454c
BenRed <64615939+BenRed06@users.noreply.github.com>
2021-09-09 10:01:43 -0400

    basic testing stuff
    working to get the basic program working. printing the results to the console. ill deal with texting later. just need to get this part done

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import time
+import requests
+import json
 
+
+#PRINTER_IP/api/telemetry
+
+res = requests.get('YOUR_PRINTERS_IP')
+
+
+stuff = res.json()
+
+while True:
+    time.sleep(60.0)
+    print(stuff)
