e50a950de1cdc2536780ca0b7d2a09f80f3078d1
BenRed <64615939+BenRed06@users.noreply.github.com>
2021-09-09 10:37:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,17 @@ import json
 
 
 #information
-name = Prusa Mini #put your printers name here (it can be anything)
-ip = YOUR_PRINTER_IP_HERE #put the IP of the printer here
-wait = 20 #set this as a decimal: example: 1.0 or 3.6, (it is in seconds)
+name = 'Prusa Mini' #put your printers name here (it can be anything)
+ip = "192.168.1.253" #put the IP of the printer here
+wait = 5 #set this as a decimal: example: 1.0 or 3.6, (it is in seconds)
 
 
+
+#DO NOT TOUCH
+hotend = []
+bed = []
+ete = []
+percent = []
 
 
 
@@ -26,7 +32,12 @@ wait = 20 #set this as a decimal: example: 1.0 or 3.6, (it is in seconds)
 
 
 while True:
-    print(name)
-    print(ip)
+    print("stats on:", {name}) #name of printer
+    print("ip", ip) 
+    print("hotend temp", ) #hotend temp
+    print("bed temp") #bed temp
+    print("est till done") #estimated time till done
+    print("percent done") #percentage done
+    print("-------------")
     time.sleep(wait)
 
